cad1cecb9ac40ba353994798cfce43d6f5bb72a7
Katehet <sv.sleep@gmail.com>
2018-02-22 23:43:39 +0300

    Added full pages and adaptive images

--- source/js/script.js
+++ source/js/script.js
@@ -5,9 +5,8 @@ var menuWrapper = document.querySelector(".main-menu");
 var menuJS = document.querySelector(".no-js-menu");
 
 menuButton.addEventListener("click", function () {
-  // menuButton.preventDefault();
   menuWrapper.classList.toggle("main-menu--opened");
-  menu.classList.toggle("mobile-only");
+  menu.classList.toggle("mobile-menu");
 });
 
 menuJS.classList.remove("no-js-menu");
