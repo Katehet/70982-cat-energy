3eef19e084a23bf332c054f3769bb278fd213f86
Katehet <sv.sleep@gmail.com>
2018-02-28 00:25:59 +0300

    Кадрирование графики. Адаптация для ретины. Оптимизация и сборка build

--- source/js/script.js
+++ source/js/script.js
@@ -11,3 +11,34 @@ menuButton.addEventListener("click", function () {
 
 menuJS.classList.remove("no-js-menu");
 jsButton.classList.remove("no-js-button");
+
+// интерактивная карта
+//Yandex version
+ymaps.ready(init);
+
+function init(){
+
+    var myMap;
+
+    myMap = new ymaps.Map('map', {
+        center: (screen.width < 1300) ? ([59.93877121663107, 30.323274149999975]) : ([59.939075, 30.31965]),
+        zoom: 17,
+        controls: []
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+
+    myMap.controls.add("zoomControl", {
+        position: {top: 100, right: 25}
+    });
+
+    var myPlacemark = new  ymaps.Placemark([59.93877121663107, 30.323274149999975] , {},
+        {
+          iconLayout: 'default#image',
+          iconImageHref: '/img/map/map-pin.png',
+          iconImageSize: (screen.width > 767) ? [124, 106] : [62, 53],
+          iconImageOffset: (screen.width > 767) ? [-62, -83] : [-31, -27.5]
+        });
+        myMap.geoObjects.add(myPlacemark);
+
+}
